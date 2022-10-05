bb3499e60cfb7d6f2f9408a0144b803b0f3807b2
VidyaraniSargar99 <113835040+VidyaraniSargar99@users.noreply.github.com>
2022-10-05 14:57:19 +0530

    changes warn

---
+++ assignment1.js
@@ -0,0 +1,11 @@
+
+console.log("Hello");
+console.log("Vidyarani Yashwant Sargar");
+console.log("My hobbies:Reading books,cooking");
+console.info("My strengths are: Hard working,Positive thinker,self confidence");
+console.warn("My weaknesses are:Emotional,Not good communication in english");
+console.log("30+50");
+console.log("49*479");
+
+
+
---
+++ demo.js
@@ -0,0 +1,6 @@
+console.log("Vidyarani Yashwant Sargar");
+console.log("My hobbies:Reading books,cooking");
+console.info("My strengths are: Hard working,Positive thinker,self confidence");
+console.warn("My weaknesses are:Emotional,Not good communication in english");
+console.log("30+50");
+console.log("49*479");
