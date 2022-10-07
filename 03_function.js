a6a308978bcf27b04b5d80e68bb3346915820a32
VidyaraniSargar99 <113835040+VidyaraniSargar99@users.noreply.github.com>
2022-10-07 13:11:42 +0530

    first commit

---
+++ 03_function.js
@@ -0,0 +1,19 @@
+
+function sachin(money){
+    
+    console.log("please go to in market with RS.:",money);
+    console.log("buy some vegetables");
+    return "vegetables bag";
+}
+
+var veg = sachin(500);
+console.log(veg);   
+
+function functionName(city,street,age){
+    //city
+    //street
+    //age
+   return "roja";
+}
+var value = functionName("pune","kolapur",22);
+console.log(value);
