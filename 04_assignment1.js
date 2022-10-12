d5b1b59133e6c70ec624739646aa2e10dede3d2a
VidyaraniSargar99 <113835040+VidyaraniSargar99@users.noreply.github.com>
2022-10-13 01:24:11 +0530

    TO add concat (hobbies)

---
+++ 04_assignment1.js
@@ -0,0 +1,15 @@
+console.log("===========QUESTION:01===================");
+console.log('"My dream company is "Infosys"');
+console.log("===========QUESTION:02==================");
+console.log("My hobbies:");
+var hobby_1 = "Watching movies";
+var hobby_2 = "Reading books";
+var hobby_3 = "Cooking";
+console.log("Hobby-1:",hobby_1);
+console.log("Hobby-2:",hobby_2);
+console.log("Hobby-3:",hobby_3);
+console.log("===========concat===========");
+console.log(hobby_1+","+hobby_2+","+hobby_3);
+
+
+
