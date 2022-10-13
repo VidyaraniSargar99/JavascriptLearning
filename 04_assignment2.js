1317bea10fd0ad44aa64d957ee99ebd70b90b8cb
VidyaraniSargar99 <113835040+VidyaraniSargar99@users.noreply.github.com>
2022-10-13 23:03:09 +0530

    TO use strings

---
+++ 04_assignment2.js
@@ -0,0 +1,54 @@
+console.log("==================1) Step 01). The given string is:====================================================");
+var stringHandsOn = "Hey you are doing good, keep it up";
+console.log("Hey you are doing good,keep it up");
+console.log("======================================================================");
+var count = stringHandsOn.length ;
+console.log("Step 02) Length of string is:",count);
+console.log("======================================================================");
+var given_string = ("Heyyouaredoinggood,keepitup");
+var spaces = given_string.length;
+console.log("Step 03) Removed extra spaces:",spaces);
+console.log("======================================================================");
+var given_string=("------------");
+var spaces = given_string.length;
+console.log("Step 04) Total number of spaces removed:",spaces);
+console.log("======================================================================");
+var index = stringHandsOn.charAt(stringHandsOn.length-1);
+console.log(" Step 05) Last character of string is:",index);
+console.log("======================================================================");
+var index = stringHandsOn.charAt(stringHandsOn.length1);
+console.log(" Fist character of string is:",index);
+console.log("======================================================================");
+var res = "Hey you are doing good keep it up";
+var stepSplit =  res.split(" ");
+console.log(stepSplit);
+var words = stepSplit.length;
+console.log("Step 06)Total words available in the string:",words);
+console.log("======================================================================");
+var words = res.indexOf('good');
+console.log(")Step 07index of word 'good' from given string is:",words);
+console.log("Step 08)======================================================================");
+console.log("=====================substring()=================================================");
+var words = res.substring(22);
+console.log(" starting from '22' is:",words);
+console.log("======================slice()================================================");
+var words = res.slice(22);
+console.log("starting from '22' is:",words);
+console.log("Step 09)================includes()======================================================");
+var str = ("is there any string ends with word up:");
+console.log("Is there any string ends with word up:");
+console.log(str.includes("up"));
+console.log("Step 10)================includes()======================================================");
+var str = ("is there any string starts with word Hey:");
+console.log("Is there any string starts with word Hey:");
+console.log(str.includes("Hey"));
+
+
+
+
+
+
+
+
+
+
