3e0df14e7e5ffc16af69cfbb62501715441a8bff
VidyaraniSargar99 <113835040+VidyaraniSargar99@users.noreply.github.com>
2022-10-15 15:11:12 +0530

    add switch(months)

---
+++ 06_switch_assignment1.js
@@ -0,0 +1,43 @@
+var month= 1;
+switch (month){
+    case 1:
+        console.log("January");
+        case 2:
+        console.log("February");
+        
+        case 3:
+        console.log("March");
+        
+        case 4:
+        console.log("April");
+        case 5:
+        console.log("May");
+        
+        case 6:
+        console.log("June");
+        case 7:
+        console.log("July");
+        case 8:
+        console.log("August");
+        
+        case 9:
+        console.log("September");
+        case 10:
+        console.log("October");
+        case 11:
+        console.log("November");
+        
+        case 12:
+        console.log("December");
+        default:console.log("please enter valid months number");
+        
+        
+}
+
+
+
+//console.log(months);
+//months start from 0 1 2 3....
+//const monthName = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+//let currentMonthName = monthNames[January];
+//console.log("The month 1 is:",currentMonthName);
